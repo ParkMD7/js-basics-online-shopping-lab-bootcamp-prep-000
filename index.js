ba3f2400651dae8c1424e3a855752a156801ec3e
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-21 20:34:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,14 +61,15 @@ function total() {
 function removeFromCart(item) {
   // write your code here
 
-  var cartItemCount = getCart().length
-  for(var i = 0; i < cartItemCount; i++) {
-    if(item == cart[i].cartItemName) {
-      cart.splice(i, 1)
-      return cart
-    }
+  var itemCount = getCart().length;
+
+  for(var i = 0; i < itemCount; i++) {
+    if(item == cart[i].itemName) {
+      cart.splice(i, 1);
+      return cart;
+    } 
   }
-  return "That item is not in your cart."
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
